3018bb2d2b574f3543a056a60196759512e027d8
Mr.Gonzales <151160699+xxGonzalesxx@users.noreply.github.com>
2025-08-09 22:11:06 +0400

    Работа с epicid

---
+++ src/Subtask.java
@@ -0,0 +1,10 @@
+public class Subtask extends Task {
+    private final int  epicId;
+    public Subtask(int id,String name, String description, Status status, int epicId){
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+    public int getEpicId(){
+        return  epicId;
+    }
+}
