61a94e1c9c1b96b45d63d57396e1eb3cd16e88fd
Mr.Gonzales <151160699+xxGonzalesxx@users.noreply.github.com>
2025-08-27 01:22:04 +0400

    Класс InMemoryHistoryManager

---
+++ src/InMemoryHistoryManager.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private final List<Task> history = new ArrayList<>();
+
+    @Override
+    public void add(Task task) {
+        if (task == null) return;
+        if (history.size() == 10) {
+            history.remove(0);
+        }
+        history.add(task);
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(history);
+    }
+}
