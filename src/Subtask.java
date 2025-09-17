22f307f21f33c9be7810db08ac3f2803d35849b1
Mr.Gonzales <151160699+xxGonzalesxx@users.noreply.github.com>
2025-09-17 19:01:46 +0400

    Исправлен код стайл Subtask.java по Checkstyle

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,10 +1,12 @@
 public class Subtask extends Task {
-    private final int  epicId;
-    public Subtask(int id,String name, String description, Status status, int epicId){
+    private final int epicId;
+
+    public Subtask(int id, String name, String description, Status status, int epicId) {
         super(id, name, description, status);
         this.epicId = epicId;
     }
-    public int getEpicId(){
-        return  epicId;
+
+    public int getEpicId() {
+        return epicId;
     }
 }
