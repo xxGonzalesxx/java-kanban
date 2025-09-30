0698796c0b3cae0f0a0a001b118d12166f668e43
Mr.Gonzales <151160699+xxGonzalesxx@users.noreply.github.com>
2025-09-30 16:11:43 +0400

    Исправлен HistoryManager под Checkstyle

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -1,10 +1,13 @@
 package managers;
 
 import tasks.Task;
-
 import java.util.List;
 
 public interface HistoryManager {
+
+    // Добавляем задачу в историю
     void add(Task task);
+
+    // Получаем историю задач
     List<Task> getHistory();
 }
